2a495ee4813428e9212d112e6751c5f2a2102950
Lam Bui <lamdbui@gmail.com>
2017-09-05 19:38:24 -0700

    Serve an HTML file and also add socket connect/disconnect handling

--- socket.io/chat_app/index.js
+++ socket.io/chat_app/index.js
@@ -1,8 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);  // socket gets initialized with the http object
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello, World!</h1>');
+  response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
 });
 
 http.listen(3000, () => {
